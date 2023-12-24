fb2ae57344cedb45e60c645c49cc837c346391f1
Storen <149369498+Leistungen@users.noreply.github.com>
2023-12-24 15:02:28 +0300

    Update conftest.py
    добавлен параметр для задания языка интерфейса, по умолчанию равный "en".

--- conftest.py
+++ conftest.py
@@ -4,17 +4,26 @@ from selenium import webdriver
 def pytest_addoption(parser):
     parser.addoption('--browser_name', action='store', default='chrome',
                      help="Choose browser: chrome or firefox")
-    
+    parser.addoption('--language', action='store', default='en',
+                     help="Choose language")
+
 @pytest.fixture(scope="function")
 def browser(request):
     browser_name = request.config.getoption("browser_name")
     browser = None
+    user_language = request.config.getoption("language")
     if browser_name == "chrome":
         print("\nstart chrome browser for test..")
-        browser = webdriver.Chrome()
+        from selenium.webdriver.chrome.options import Options
+        options = Options()
+        options.add_experimental_option('prefs', {'intl.accept_languages': user_language})
+        browser = webdriver.Chrome(options=options)
     elif browser_name == "firefox":
         print("\nstart firefox browser for test..")
-        browser = webdriver.Firefox()
+        from selenium.webdriver.firefox.options import Options
+        options = Options()
+        options.set_preference("intl.accept_languages", user_language)
+        browser = webdriver.Firefox(options=options)
     else:
         raise pytest.UsageError("--browser_name should be chrome or firefox")
     yield browser
