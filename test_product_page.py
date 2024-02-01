430c1e0cb170777a749fd80e3201571bdc1b4b9d
Stören <149369498+Leistungen@users.noreply.github.com>
2024-02-01 15:53:23 +0300

    add fixture Setup
    новый тестовый класс TestUserAddToBasketFromProductPage с  фикстурой setup

--- test_product_page.py
+++ test_product_page.py
@@ -1,6 +1,8 @@
+import time
+import pytest
 from .pages.product_page import ProductPage
 from .pages.basket_page import BasketPage
-import pytest
+from .pages.login_page import LoginPage
 
 
 @pytest.mark.skip
@@ -35,6 +37,7 @@ def test_guest_can_go_to_login_page_from_product_page(browser):
     page.open()
     page.go_to_login_page()
 
+@pytest.mark.skip
 def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
     page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
@@ -43,3 +46,27 @@ def test_guest_cant_see_product_in_basket_opened_from_product_page(browser):
     basket_page = BasketPage(browser, browser.current_url)
     basket_page.should_be_emptybasket_message()
     basket_page.should_not_be_products_in_basket()
+
+class TestUserAddToBasketFromProductPage():
+
+    @pytest.fixture(scope="function", autouse=True)
+    def setup(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/en-gb/accounts/login/"
+        page = LoginPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()
+        email = str(time.time()) + "@fakemail.org"
+        page.register_new_user(email, email)
+        page.should_be_authorized_user()
+        
+    def test_user_cant_see_success_message(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+        page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()                      # открываем страницу
+        page.should_not_be_success_message()
+    
+    def test_user_can_add_product_to_basket(self, browser):
+        link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207"
+        page = ProductPage(browser, link)   # инициализируем Page Object, передаем в конструктор экземпляр драйвера и url адрес 
+        page.open()                      # открываем страницу
+        page.add_to_basket()
+        page.check_basket()
